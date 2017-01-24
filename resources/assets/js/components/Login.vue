334e040f1e798dd696fb4f45cd5ecdf10efac7d8
Jack Roper <roper.jack.h@gmail.com>
2017-01-24 01:51:46 +0000

    Added container around login form

--- resources/assets/js/components/Login.vue
+++ resources/assets/js/components/Login.vue
@@ -1,37 +1,39 @@
 <template>>
-  <form id="login" class="form-horizontal">
-    <fieldset>
-      
-      <!-- Login legend -->
-      <legend>Login</legend>
-      
-      <!-- Email -->
-      <div class="form-group">
-        <label class="col-md-4 control-label" for="email">Email</label>  
-        <div class="col-md-4">
-          <input id="email" name="email" type="text" placeholder="inno.flow@email.com" class="form-control input-md" required>
+  <div class="container">
+    <form id="login" class="form-horizontal">
+      <fieldset>
+        
+        <!-- Login legend -->
+        <legend>Login</legend>
+        
+        <!-- Email -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="email">Email</label>  
+          <div class="col-md-4">
+            <input id="email" name="email" type="text" placeholder="inno.flow@email.com" class="form-control input-md" required>
+          </div>
         </div>
-      </div>
-      
-      <!-- Password -->
-      <div class="form-group">
-        <label class="col-md-4 control-label" for="password">Password</label>
-        <div class="col-md-4">
-          <input id="password" name="password" type="password" placeholder="password" class="form-control input-md" required>
+        
+        <!-- Password -->
+        <div class="form-group">
+          <label class="col-md-4 control-label" for="password">Password</label>
+          <div class="col-md-4">
+            <input id="password" name="password" type="password" placeholder="password" class="form-control input-md" required>
+          </div>
         </div>
-      </div>
-
-      <div class="form-group">
-        <div class="col-md-4 col-md-offset-4">
-          <button type="button" class="btn">Login</button>
+  
+        <div class="form-group">
+          <div class="col-md-4 col-md-offset-4">
+            <button type="button" class="btn">Login</button>
+          </div>
+          <br><br>
+          <div class="col-md-4 col-md-offset-4">
+            <p>Need an account? <router-link to="/register">Register</router-link></p>
+          </div>
         </div>
-        <br><br>
-        <div class="col-md-4 col-md-offset-4">
-          <p>Need an account? <router-link to="/register">Register</router-link></p>
-        </div>
-      </div>
-    </fieldset>
-  </form>
+      </fieldset>
+    </form>
+  </div>
 </template>
 
 <script>
