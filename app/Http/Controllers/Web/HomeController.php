1bd9712191f78fff0fe0686ddd1c31fdb401ed30
SickAustrian <SickAustrian@users.noreply.github.com>
2017-01-31 17:22:50 +0000

    redirect bug fix

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -26,6 +26,6 @@ class HomeController extends Controller
     {
         $service -> requestToken( $request -> all() );
 
-        return view( 'index' ) -> with( [ 'waitForToken' => true ] );
+        return redirect( 'index' ) -> with( [ 'waitForToken' => true ] );
     }
 }
