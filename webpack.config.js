b251aaa609d00bbdb88005f68032dc28ba45db8c
Jack Roper <roper.jack.h@gmail.com>
2017-02-01 22:50:20 +0000

    Added karma tests

---
+++ webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        loader: 'babel-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader'
+      }
+    ]
+  },
+  resolve: {
+    alias: {
+      vue: 'vue/dist/vue.js'
+    }
+  }
+}
