201ea869a0a3164d32d6bfbc7efff99342a27979
SickAustrian <SickAustrian@users.noreply.github.com>
2017-02-20 14:40:11 +0100

    database

--- config/database.php
+++ config/database.php
@@ -67,26 +67,15 @@ return [
         ],
         'pgsql' => [
             'driver' => 'pgsql',
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $host,
+            'database' => $database,
+            'username' => $username,
+            'password' => $password,
             'charset' => 'utf8',
             'prefix' => '',
             'schema' => 'public',
             'sslmode' => 'prefer',
-        ],
-        'heroku' => [
-            'driver'   => 'pgsql',
-            'host'     => $host,
-            'database' => $username,
-            'username' => $password,
-            'password' => $database,
-            'charset'  => 'utf8',
-            'prefix'   => '',
-            'schema'   => 'public',
-        ],
+        ]
     ],
     /*
     |--------------------------------------------------------------------------
