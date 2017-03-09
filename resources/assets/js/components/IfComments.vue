5e981801c5b396e91508e9a7eeb4071ca4425c2c
1f604 <1f604@users.noreply.github.com>
2017-03-09 14:10:36 +0000

    tidied up code for comments.vue

--- resources/assets/js/components/IfComments.vue
+++ resources/assets/js/components/IfComments.vue
@@ -1,53 +1,36 @@
 <template>             
-<div class="comment">
+  <div class="comment">
 
-
-  <div class="col-lg-12">
-    <h1>Commits</h1>              
-    <div v-for="i in commits">
-      {{i.id}}: {{i.comment}} <br>           
-      by {{i.commiter.username}} ({{i.date}}) <br><br>
-    </div>            
-  </div>
+    <div class="col-lg-12">
+      <h1>Commits</h1>              
+      <div v-for="i in commits">
+        {{i.id}}: {{i.comment}} <br>           
+        by {{i.commiter.username}} ({{i.date}}) <br><br>
+      </div>            
+    </div>
 
     <if-message ref="message"></if-message>
-  <div >
-    <h1>Comments</h1>              
-            <div v-for="i in comments" style=".if-card { width: 100%; height: 50%;  
-              /* These are technically the same, but use both */
-  overflow-wrap: break-word;
-  word-wrap: break-word;
+    
+    <div >
+          <h1>Comments</h1>       
 
-  -ms-word-break: break-all;
-  /* This is the dangerous one in WebKit, as it breaks things wherever */
-  word-break: break-all;
-  /* Instead use this non-standard one: */
-  word-break: break-word;
-
-  /* Adds a hyphen where the word breaks, if supported (No Blink) */
-  -ms-hyphens: auto;
-  -moz-hyphens: auto;
-  -webkit-hyphens: auto;
-  hyphens: auto;}" >
+          <div v-for="i in comments" style="" >
               <if-card> 
                 <span class="h3">{{i.text}}</span>                            
                 <br>by {{i.owner.username}} ({{i.date}})
               </if-card>
-            </div> 
+          </div> 
 
-              <if-card>       
-                <b>Message</b>
-                <br>
-                <textarea v-model="newComment" style="box-sizing:border-box" placeholder="20+ chars"></textarea>
-                <br>
-     
-                  <a  href="#" v-on:click="submitComment($event)">Submit Comment</a>  
-              </if-card>         
-
-  </div>
-
-
-</div> 
+          <if-card>       
+            <b>Message</b>
+            <br>
+            <textarea v-model="newComment" style="box-sizing:border-box" placeholder="20+ chars"></textarea>
+            <br>
+            <a  href="#" v-on:click="submitComment($event)">Submit Comment</a>  
+          </if-card>         
+    </div>
+  
+  </div> 
 </template>
 
 <script>
@@ -66,25 +49,25 @@
 
       // Innovation markup snippets
       comments: [
-  {
-    "id": 4,
-    "date": "2017-03-04 18:13:18",
-    "text": "This is a comment. Always make sure a comment is useful.",
-    "owner": {
-      "id": 101,
-      "username": "SickAustrian"
-    }
-  },
-  {
-    "id": 3,
-    "date": "2017-03-04 17:26:39",
-    "text": "This is a comment. Always make sure a comment is useful.",
-    "owner": {
-      "id": 3,
-      "username": "JackRoper"
-    }
-  },
-]
+        {
+          "id": 4,
+          "date": "2017-03-04 18:13:18",
+          "text": "This is a comment. Always make sure a comment is useful.",
+          "owner": {
+            "id": 101,
+            "username": "SickAustrian"
+          }
+        },
+        {
+          "id": 3,
+          "date": "2017-03-04 17:26:39",
+          "text": "This is a comment. Always make sure a comment is useful.",
+          "owner": {
+            "id": 3,
+            "username": "JackRoper"
+          }
+        },
+      ]
     }
   }
 
@@ -99,31 +82,21 @@
       IfMessage
     },
 
-  props: [
-    'projectID',
-    'codeReviewID' 
-  ],
+    props: [
+      'projectID',
+      'codeReviewID' 
+    ],
 
     data() {
       return defaultCommentsData();
     },
  
-    beforeRouteEnter(to, from, next) {
-      console.log(to);  
+    beforeRouteEnter(to, from, next) { 
       next(commentsComponent => {
         commentsComponent.init(to.params.id, to.params.codeReviewID);
       }); 
-    },
+    }, 
 
-/*    beforeRouteUpdate (to, from, next) {
-      if (typeof this != 'undefined'){
-        if (to.params.id !== this.projectID) {
-          this.init(to.params.id, to.params.codeReviewID);
-        }
-      }
-      next();
-    },
-*/
     created() { 
       this.loadCommitsAndComments();
     },
@@ -137,14 +110,13 @@
         return true;
       }
     },
+
     methods: {
-      init(projectID, codeReviewID) {
-        console.log("running init");
+      init(projectID, codeReviewID) { 
         this.projectID = projectID;
         this.codeReviewID = codeReviewID;
         this.loadDetails(); 
-      },
-      // Request innovations
+      }, 
 
       loadDetails() {
         if (this.projectID) {
@@ -166,18 +138,11 @@
       },
 
 
-      loadCommitsAndComments() { 
-//          axios.get('http://innoflow.app/api/projects/07fa8c3e-546d-4e7a-8edb-4274a9e631c3/codereviews/1/comments')
+      loadCommitsAndComments() {  
         axios.get('api/projects/' + this.projectID +'/codereviews/' + this.codeReviewID)
-          .then((res) => {
-//            this.comments = res.data.comments;
-console.log(res.data);
-              this.comments = res.data.comments;
-              this.commits = res.data.commits;
-console.log("comments and commits has been set");
-
-console.log(this.commits);
-console.log(this.comments);
+          .then((res) => { 
+            this.comments = res.data.comments;
+            this.commits = res.data.commits; 
 
           })
           .catch((error) => {
@@ -190,9 +155,7 @@ console.log(this.comments);
 
       submitComment(e) {
         e.preventDefault();
-        if (this.validCodeReviewFields) { 
-          console.log("projectID:"+this.projectID)
-          console.log("commitID:"+this.codeReviewID)
+        if (this.validCodeReviewFields) {  
           axios.post('api/projects/' + this.projectID + '/codereviews/' + this.codeReviewID + '/comments', { projectID: this.projectID, codeReviewId: this.codeReviewID, message: this.newComment })
             .then((res) => {
               console.log('Succeeded in submitting comment.');
@@ -201,7 +164,6 @@ console.log(this.comments);
               console.log(error);
               console.log('Failed to submit comment.');
             });
- 
         }        
       }
 
@@ -210,4 +172,22 @@ console.log(this.comments);
 </script>
 
 <style scoped> 
+.if-card { 
+  width: 100%; height: 50%;  
+  /* These are technically the same, but use both */
+  overflow-wrap: break-word;
+  word-wrap: break-word;
+
+  -ms-word-break: break-all;
+  /* This is the dangerous one in WebKit, as it breaks things wherever */
+  word-break: break-all;
+  /* Instead use this non-standard one: */
+  word-break: break-word;
+
+  /* Adds a hyphen where the word breaks, if supported (No Blink) */
+  -ms-hyphens: auto;
+  -moz-hyphens: auto;
+  -webkit-hyphens: auto;
+  hyphens: auto;
+}
 </style>
